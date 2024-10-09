21a2b3c0c72b8d6a90c4775bd3991af28c46d0b4
jacobLi <jacob.li@ferguson.com>
2024-10-09 22:28:20 +0800

    update

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -29,7 +29,7 @@ export default function Home() {
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-        <a href="https://next-demo-rose-seven.vercel.app/buy">Open in App</a>
+        <a className="mb-4" href="https://next-demo-rose-seven.vercel.app/buy/otherpage">Open in App</a>
 
           <li className="mb-2" onClick={handleClick}>
             this is a test
