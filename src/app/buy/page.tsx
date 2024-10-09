5a903cc12b00db0f0deaf7d6d86d413cc0bd14b5
jacobLi <jacob.li@ferguson.com>
2024-10-09 22:06:02 +0800

    update

--- src/app/buy/page.tsx
+++ src/app/buy/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
     }, 2000);
   
     // 尝试通过 Universal Link 打开 App
-    window.location.href = appLink;
+    window.open(appLink)
   
     // 监听页面可见性，如果用户返回了页面，则清除定时器
     document.addEventListener('visibilitychange', function() {
