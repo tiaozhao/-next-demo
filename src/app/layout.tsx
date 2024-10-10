47e84801645b48eb32f8d2d67a9eb94442d7d923
jacobLi <jacob.li@ferguson.com>
2024-10-10 14:14:52 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,6 +25,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta content="app-id=1173029601" name="apple-itunes-app"></meta>
+      </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
