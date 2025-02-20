d337e9d5715e4c0ec3b88b1d87f49bc749585195
Jacob Li <jacob.li@guitarcenter.com>
2025-02-20 15:34:31 +0800

    update universal link config

---
+++ src/app/cart/page.tsx
@@ -0,0 +1,7 @@
+export default function CartPage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <h1 className="text-2xl">Cart Page</h1>
+    </div>
+  );
+} 
