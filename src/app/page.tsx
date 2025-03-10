b7b6e5e22a757d7b0511fbc86454eab2e83f391d
Jacob Li <jacob.li@guitarcenter.com>
2025-03-10 15:47:33 +0800

    update

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,13 @@ import Image from "next/image";
 export default function Home() {
 
   return (
+    <div id="BodyWrap"> 
+    <h1>On Sale</h1> 
+    <p>Good News - The Platinum Martin Standard Series D-28 Dreadnought Acoustic Guitar Natural has just dropped in price!</p> 
+    <p><button id="ViewProductButton" type="button" value="GuitarCenterApp://ProductDetailsPage?ID=site5127474188253093646" >View Product</button></p> 
+    </div> 
+  )
+  return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
         <Image
