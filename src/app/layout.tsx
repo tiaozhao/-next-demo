96174fbf19b5e75f089517ffa409ed24662b8744
Jacob Li <jacob.li@guitarcenter.com>
2025-03-10 15:49:07 +0800

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -41,13 +41,7 @@ export default async function RootLayout({
 <head>
   <meta charSet="utf-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-</head>
-<style> 
- #BodyWrap { font-family:Arial, Helvetica, sans-serif; font-size:14px; line-height:18px; padding:2%; } 
- #BodyWrap H1 { margin-bottom:20px; } 
- #BodyWrap P { margin-bottom:20px; } 
- #ViewProductButton { background-color:#CC0000; width:100%; max-width:500px; text-align:center; padding:10px; margin:auto; color:#ffffff; border:0 none;  border-radius:5px; font-size:20px; display:block; font-weight:bold; } 
- </style> 
+</head> 
  <body className="antialiased">
   <main className="main-content">
     {children}
