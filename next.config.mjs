5a7ce1d229ebdc78893894b30663d9ce711d9f4e
Jacob Li <jacob.li@guitarcenter.com>
2025-06-13 10:56:25 +0800

    add cache

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
   images: {
     domains: ['b2b-accelerator.myshopify.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=2592000, stale-while-revalidate=86400',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
