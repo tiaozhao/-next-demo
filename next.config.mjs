bf67a8129c2ab54ca34547ce64758e6f6362e9a9
Jacob Li <jacob.li@guitarcenter.com>
2025-06-13 13:50:09 +0800

    update

--- next.config.mjs
+++ next.config.mjs
@@ -10,7 +10,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Cache-Control',
-            value: 'public, max-age=60, stale-while-revalidate=30',
+            value: 'public, max-age=60, stale-while-revalidate=30, s-maxage=2592000',
           },
         ],
       },
